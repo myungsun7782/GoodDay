9defacae1128a360315ce3cb8f9306ad317640a4
myungsun <audtjs9987@naver.com>
2022-03-03 16:46:05 +0900

    Initial Commit

---
+++ GoodDayApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  GoodDayApp
+//
+//  Created by myungsun on 2022/03/03.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
